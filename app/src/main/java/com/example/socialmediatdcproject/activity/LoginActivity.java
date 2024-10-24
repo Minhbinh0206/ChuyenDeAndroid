8d21800e25eee585d3473eedf148a44ac35d3092
Hien <22211tt4044@mail.tdc.edu.vn>
2024-10-24 14:26:02 +0700

    Login with Verification

--- app/src/main/java/com/example/socialmediatdcproject/activity/LoginActivity.java
+++ app/src/main/java/com/example/socialmediatdcproject/activity/LoginActivity.java
@@ -8,12 +8,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 import com.example.socialmediatdcproject.API.StudentAPI;
 import com.example.socialmediatdcproject.R;
+import com.example.socialmediatdcproject.admin.AdminActivity;
 import com.example.socialmediatdcproject.model.Student;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,22 +28,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import java.util.List;
-
-
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    boolean isLoggin = false;
     private ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.login_layout);
 
         mAuth = FirebaseAuth.getInstance(); // Khởi tạo FirebaseAuth
+        initUi();
 
         // Kiểm tra xem người dùng đã đăng nhập hay chưa
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -53,62 +49,28 @@ public class LoginActivity extends AppCompatActivity {
             studentAPI.getStudentByKey(currentUser.getUid(), new StudentAPI.StudentCallback() {
                 @Override
                 public void onStudentReceived(Student student) {
-                    isLoggin = true;
                     Log.d("TAG", "User: " + student.getFullName());
 
                     if (student.getUserId() != -1) {
-                        // Nếu đã đăng nhập, chuyển thẳng đến SharedActivity
+                        // Nếu đã đăng nhập, chuyển đến SharedActivity
                         Intent intent = new Intent(LoginActivity.this, SharedActivity.class);
                         startActivity(intent);
                         finish(); // Đóng LoginActivity để không quay lại
-                    }
-                    else {
-                        // Người dùng chưa đăng nhập, chuyển qua màn hình UploadProfileActivity
-                        Intent intent = new Intent(LoginActivity.this, UploadProfileActivity.class);
-                        startActivity(intent);
-                        finish(); // Đóng LoginActivity để không quay lại
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Vui lòng đăng nhập", Toast.LENGTH_SHORT).show();
                     }
                 }
 
                 @Override
-                public void onStudentsReceived(List<Student> students) {
-
-                }
+                public void onStudentsReceived(List<Student> students) {}
 
                 @Override
-                public void onError(String errorMessage) {
-
-                }
+                public void onError(String errorMessage) {}
 
                 @Override
-                public void onStudentDeleted(int studentId) {
-
-                }
+                public void onStudentDeleted(int studentId) {}
             });
-
-        } 
-        
-
-        // Thiết kế giao diện
-        avatar = findViewById(R.id.image_logo);
-        Glide.with(this)
-                .load(R.drawable.image_logo_login)
-                .circleCrop()
-                .into(avatar);
-
-        // Chuyển qua trang đăng ký
-        TextView textSignUp = findViewById(R.id.textSignUp);
-        textSignUp.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        });
-
-        // Chuyển qua trang quên mật khẩu
-        TextView textForgotPassword = findViewById(R.id.textForgotPassword);
-        textForgotPassword.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
-            startActivity(intent);
-        });
+        }
 
         // Xử lý đăng nhập
         Button btnLogin = findViewById(R.id.btnLogin);
@@ -144,7 +106,6 @@ public class LoginActivity extends AppCompatActivity {
                                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                                 if (snapshot.exists()) {
                                                     for (DataSnapshot userSnapshot : snapshot.getChildren()) {
-                                                        // Lấy userId và roleId từ dữ liệu của người dùng
                                                         int userId = userSnapshot.child("userId").getValue(Integer.class);
                                                         int roleId = userSnapshot.child("roleId").getValue(Integer.class);
 
@@ -154,23 +115,18 @@ public class LoginActivity extends AppCompatActivity {
                                                             @Override
                                                             public void onDataChange(@NonNull DataSnapshot roleSnapshot) {
                                                                 String roleName = roleSnapshot.child("roleName").getValue(String.class);
-                                                                // Mã kiểm tra vai trò admin
                                                                 Set<Integer> adminRoleIds = new HashSet<>(Arrays.asList(2, 3, 4, 5));
                                                                 boolean isAdmin = adminRoleIds.contains(roleId);
 
-                                                                for (int id : adminRoleIds) {
-                                                                    if (roleId == id) {
-                                                                        isAdmin = true ;
-                                                                        break;
-                                                                    }
-                                                                }
-
                                                                 if (isAdmin) {
                                                                     Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+                                                                    intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                                                                     startActivity(intent);
                                                                 } else {
                                                                     Intent intent = new Intent(LoginActivity.this, SharedActivity.class);
+//                                                                    intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                                                                     startActivity(intent);
+                                                                    finish();
                                                                 }
                                                                 finish();
                                                             }
@@ -196,6 +152,29 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Đăng nhập thất bại: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
+        });
+    }
+
+    private void initUi() {
+        // Thiết kế giao diện
+        avatar = findViewById(R.id.image_logo);
+        Glide.with(this)
+                .load(R.drawable.image_logo_login)
+                .circleCrop()
+                .into(avatar);
+
+        // Chuyển qua trang đăng ký
+        TextView textSignUp = findViewById(R.id.textSignUp);
+        textSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        // Chuyển qua trang quên mật khẩu
+        TextView textForgotPassword = findViewById(R.id.textForgotPassword);
+        textForgotPassword.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+            startActivity(intent);
         });
     }
 }
