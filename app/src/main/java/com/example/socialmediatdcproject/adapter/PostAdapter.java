2a51684fccba805b1f8441a661f73f7213e8c380
Minhbinh0206 <haminh4235@gmail.com>
2024-10-28 15:02:13 +0700

    Update Message

--- app/src/main/java/com/example/socialmediatdcproject/adapter/PostAdapter.java
+++ app/src/main/java/com/example/socialmediatdcproject/adapter/PostAdapter.java
@@ -15,12 +15,15 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.socialmediatdcproject.API.GroupAPI;
 import com.example.socialmediatdcproject.API.LikeAPI;
 import com.example.socialmediatdcproject.API.PostAPI;
 import com.example.socialmediatdcproject.API.StudentAPI;
 import com.example.socialmediatdcproject.API.UserAPI;
 import com.example.socialmediatdcproject.R;
 import com.example.socialmediatdcproject.activity.CommentPostActivity;
+import com.example.socialmediatdcproject.activity.GroupDetaiActivity;
+import com.example.socialmediatdcproject.model.Group;
 import com.example.socialmediatdcproject.model.Post;
 import com.example.socialmediatdcproject.model.Student;
 import com.example.socialmediatdcproject.model.User;
@@ -209,6 +212,7 @@ public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                                         .load(student.getAvatar())
                                         .circleCrop()
                                         .into(holder.postAvatar);
+
                             }
                         }
 
@@ -238,6 +242,47 @@ public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                     }
                 }
             }
+        });
+        holder.postAdminUserId.setOnClickListener(v -> {
+            GroupAPI groupAPI = new GroupAPI();
+            groupAPI.getGroupById(post.getGroupId(), new GroupAPI.GroupCallback() {
+                @Override
+                public void onGroupReceived(Group group) {
+                    Intent intent = new Intent(v.getContext(), GroupDetaiActivity.class);
+                    intent.putExtra("groupId", group.getGroupId());
+                    if (v.getContext() instanceof GroupDetaiActivity) {
+
+                    }else {
+                        v.getContext().startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onGroupsReceived(List<Group> groups) {
+
+                }
+            });
+        });
+
+        holder.postAvatar.setOnClickListener(v -> {
+            GroupAPI groupAPI = new GroupAPI();
+            groupAPI.getGroupById(post.getGroupId(), new GroupAPI.GroupCallback() {
+                @Override
+                public void onGroupReceived(Group group) {
+                    Intent intent = new Intent(v.getContext(), GroupDetaiActivity.class);
+                    intent.putExtra("groupId", group.getGroupId());
+                    if (v.getContext() instanceof GroupDetaiActivity) {
+
+                    }else {
+                        v.getContext().startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onGroupsReceived(List<Group> groups) {
+
+                }
+            });
         });
 
         setupLikeButton(holder, post);
